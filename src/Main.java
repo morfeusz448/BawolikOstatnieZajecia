0d659b300613706aa25baff267d2002579e5f226
morfeusz448 <krzysztofbawolik@gmail.com>
2022-01-17 15:27:01 +0100

    //dodane

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // Utworzenie 2 objektów 2 klas
         HomeAnimal<Dog> animal1 = new HomeAnimal<>(new Dog("Piesek"));
         HomeAnimal<Cat> animal2 = new HomeAnimal<>(new Cat("Kotek"));
+        //Wywołanie 2 metod pokazujących typ
         animal1.showType();
         animal2.showType();
+        //Wywołanie 2 metod spania na kanapie
         animal1.sleepOnCouch();
         animal2.sleepOnCouch();
     }
